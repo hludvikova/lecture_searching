c0208fb2412a786634ea9b595b5cbcc0aaa5281d
Ludvíková Hana (257023) <257023@vutbr.cz>
2024-04-08 12:37:41 +0200

    pokus-2

--- searching.py
+++ searching.py
@@ -2,7 +2,7 @@ import json
 import os
 import json
 
-template = "GA"
+template = ["G", "A"]
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -14,10 +14,25 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+    save=[]
     with open(file_name) as data_file:
         data = json.load(data_file)
+    i=0
     for i in data[2]:
-        
+        if i == template[0]:
+            i+=1
+            if i == template[1]:
+                x = int(len(data[2])/2)
+                if len(data[2])%2!=0:
+                    save.append(data[x])
+                else:
+                    continue
+            else:
+                continue
+        else:
+            continue
+    i += 1
+
 
 
 
